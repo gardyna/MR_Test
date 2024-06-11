f51d37581f9967865d93489ddfdac378d5a424a0
Dagur <dagur@wewanttoknow.com>
2024-06-11 14:46:40 +0200

    info popup

--- Assets/InfoBoxGrabable.cs
+++ Assets/InfoBoxGrabable.cs
@@ -1,27 +1,31 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+#nullable enable
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
 
 [RequireComponent(typeof(XRGrabInteractable))]
 public class InfoBoxGrabable : MonoBehaviour
 {
-    
+    [SerializeField] private GameObject infoPopup = null!;
     private void Start()
     {
         var grabber = GetComponent<XRGrabInteractable>();
         grabber.selectEntered.AddListener(OnGrabbed);
-        grabber.selectExited.AddListener(OnGrabbStop);
+        grabber.selectExited.AddListener(OnGrabStop);
     }
 
-    public void OnGrabbed(SelectEnterEventArgs args)
+    private void OnGrabbed(SelectEnterEventArgs args)
     {
         Debug.Log($"I've been grabbed, {args.interactorObject.transform.gameObject.tag}");
+        if (!args.interactorObject.transform.CompareTag("place"))
+        {
+            infoPopup.SetActive(true);
+        }
     }
 
-    public void OnGrabbStop(SelectExitEventArgs arg0)
+    private void OnGrabStop(SelectExitEventArgs arg0)
     {
         Debug.Log("Exit");
+        infoPopup.SetActive(false);
     }
 }
