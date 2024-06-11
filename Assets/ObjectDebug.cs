703c81c157ab11c2666e55b24e4086b0279a4c86
Dagur <dagur@wewanttoknow.com>
2024-06-11 14:02:45 +0200

    fix origin

--- Assets/ObjectDebug.cs
+++ Assets/ObjectDebug.cs
@@ -1,19 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DebugHelper : MonoBehaviour
+public class ObjectDebug : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"objects {transform.position}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"Headset: {transform.position}");
+        
     }
 }
